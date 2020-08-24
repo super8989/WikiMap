108b98baa247b8832793cfac38dd4e50cb7e684d
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-08-24 02:51:41 +0000

    Moved all registration to register.js, added resource to server.js

---
+++ routes/register.js
@@ -0,0 +1,56 @@
+// All routes for registration process are defined here.
+
+const express = require('express');
+const router  = express.Router();
+const { getUserByUsername, getUserByEmail, addUser } = require('./helpers');
+
+module.exports = (db) => {
+
+  // GET /register if no user is logged in. If user is logged in, redirect to /maps.
+  router.get('/register', (req, res) => {
+    const userID = req.session && req.session.user_id;
+    if (!userID) {
+      res.render('register');
+    } else {
+      res.redirect('/maps');
+    }
+  });
+
+  // POST to /register only if new user with valid username, email and password. Checks if username and email have previously been registered via helper functions getUserByUsername and getUserByEmail in helpers.js file. If registration successful, adds new user to database via helper function addUser in helpers.js file and redirects to /maps (for now, can be changed to redirect to user's profile or create new map, etc. later.)
+  router.post('/register', (req, res) => {
+    const user = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    };
+    const templateVars = {};
+    if (user.username === '' || user.email === '' || user.password === '') {
+      res.statusCode = 400;
+      templateVars.message = 'Oops, you left the username, email and/or password field(s) blank. Please try again.';
+      res.render('400', templateVars);
+    } else {
+      getUserByUsername(db, user.username)
+      .then(existingUser => {
+        if (existingUser) {
+          res.statusCode = 400;
+          templateVars.message = 'Sorry, that username is already registered.';
+          res.render('400', templateVars)
+        } else {
+          getUserByEmail(db, user.email)
+          .then(existingUser => {
+            if (existingUser) {
+              res.statusCode = 400;
+              templateVars.message = 'Sorry, that email is already registered.';
+              res.render('400', templateVars);
+            } else {
+              addUser(db, user);
+              res.redirect('/maps');
+            }
+          });
+        }
+      });
+    }
+  });
+
+  return router;
+};
--- server.js
+++ server.js
@@ -34,12 +34,13 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
+const registerRoutes = require("./routes/register");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/", usersRoutes(db));
+app.use("/api/users", usersRoutes(db));
 // Note: mount other resources here, using the same pattern above
-
+app.use("/", registerRoutes(db));
 
 // Home page
 // Warning: avoid creating more routes in this file!
